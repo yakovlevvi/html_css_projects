2169ed8433d75ff11959636aa6d54147360faf35
Vitaliy Yakovlev <vitaliy@MacBook-Pro-Vitaliy.local>
2024-07-18 20:38:32 +0300

    8-wm-3 done

--- Stage-8/8-WM-3/js/index.js
+++ Stage-8/8-WM-3/js/index.js
@@ -1,6 +1,6 @@
 const infoEl = document.querySelector('.info');
-const btnListEl = document.querySelector('.main-nav__menu-btn--list')
-const btnGridEl = document.querySelector('.main-nav__menu-btn--grid')
+const btnListEl = document.querySelector('.tag__menu-btn--list')
+const btnGridEl = document.querySelector('.tag__menu-btn--grid')
 
 
 btnListEl.addEventListener('click', function (e) {
@@ -12,3 +12,10 @@ btnGridEl.addEventListener('click', function (e) {
 	infoEl.classList.add('info--grid')
 	infoEl.classList.remove('info--list')
 })
+
+const menuBtnEl = document.querySelector('.header__menu');
+const headerEl = document.querySelector('.header');
+
+menuBtnEl.addEventListener('click', function (e) {
+	headerEl.classList.toggle('header--open')
+});
