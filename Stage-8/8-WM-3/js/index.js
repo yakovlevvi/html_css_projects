1ed5909aa9f2d19accda338fb5fd21993d663adf
Vitaliy Yakovlev <vitaliy@MacBook-Pro-Vitaliy.local>
2024-07-19 18:53:51 +0300

    8-wm-3 redone

--- Stage-8/8-WM-3/js/index.js
+++ Stage-8/8-WM-3/js/index.js
@@ -1,21 +1,30 @@
-const infoEl = document.querySelector('.info');
-const btnListEl = document.querySelector('.tag__menu-btn--list')
-const btnGridEl = document.querySelector('.tag__menu-btn--grid')
+const infoListEl = document.querySelector('.info__list')
+const btnListEl = document.querySelector('#btn-list')
+const btnGridEl = document.querySelector('#btn-grid')
 
+const newsCardEls = document.querySelectorAll('.news-card')
 
 btnListEl.addEventListener('click', function (e) {
-	infoEl.classList.add('info--list')
-	infoEl.classList.remove('info--grid')
-});
-
-btnGridEl.addEventListener('click', function (e) {
-	infoEl.classList.add('info--grid')
-	infoEl.classList.remove('info--list')
+	infoListEl.classList.remove('info__list--grid')
+	btnListEl.classList.add('tag__menu-btn--active')
+	btnGridEl.classList.remove('tag__menu-btn--active')
+	newsCardEls.forEach((newsCardEl) => {
+		newsCardEl.classList.remove('news-card--grid')
+	})
 })
 
-const menuBtnEl = document.querySelector('.header__menu');
-const headerEl = document.querySelector('.header');
+btnGridEl.addEventListener('click', function (e) {
+	infoListEl.classList.add('info__list--grid')
+	btnListEl.classList.remove('tag__menu-btn--active')
+	btnGridEl.classList.add('tag__menu-btn--active')
+	newsCardEls.forEach((newsCardEl) => {
+		newsCardEl.classList.add('news-card--grid')
+	})
+})
+
+const menuBtnEl = document.querySelector('.header__menu')
+const headerEl = document.querySelector('.header')
 
 menuBtnEl.addEventListener('click', function (e) {
 	headerEl.classList.toggle('header--open')
-});
+})
